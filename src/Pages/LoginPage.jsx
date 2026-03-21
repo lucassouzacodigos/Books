c08ab987aafb8280c555e271ad3f07b84305bfa9
devth3us <manoguial218@gmail.com>
2026-03-20 22:18:01 -0300

    hotfix

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -7,13 +7,53 @@ import '../App.css'
 import InputNomeado from '../Components/InputNomeado';
 import foto from '../assets/123.png'
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { toast, Bounce } from 'react-toastify';
+import api from '../services/api';
 
 
 export default function LoginPage() {
     const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+
+
 
     const login = async () => {
-        alert("Logou")
+
+      try{
+      const response = await api.post("/login", {
+        email: email,
+        password: senha
+      })
+      toast.success(response.data.response, {
+          position: "bottom-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+    }
+      catch(err){
+        console.log(err)
+        toast.warn(err.response.data.response, {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+      }
+
+
     }
 
     const cadastro = async () => {
@@ -31,8 +71,8 @@ export default function LoginPage() {
 
       <div className='loginBox'>
         <p className='tituloLogin'>ALUNOID BOOKS</p>
-        <InputNomeado largura={380} titulo={"Email:"}></InputNomeado>
-        <InputNomeado type={"password"} largura={380} titulo={"Senha:"}></InputNomeado>
+        <InputNomeado valor={setEmail} largura={380} titulo={"Email:"}></InputNomeado>
+        <InputNomeado valor={setSenha} type={"password"} largura={380} titulo={"Senha:"}></InputNomeado>
 
         <button className='botao' onClick={login} style={{backgroundColor:"black", marginTop:20, color:"white", fontWeight:"bold"}}>Entrar!</button>
         
